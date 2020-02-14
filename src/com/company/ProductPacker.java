40f1087d82948a0e335b2cc6420a69faad76854d
ForSureNotKobe <j.porebski0@gmail.com>
2020-02-14 22:43:40 +0100

    added Bag class

--- src/com/company/ProductPacker.java
+++ src/com/company/ProductPacker.java
@@ -7,9 +7,9 @@ public class ProductPacker {
 
     ArrayList<ShoppingList> shoppingList = new ArrayList<>();
     ArrayList<Product> listOfProducts = new ArrayList<>();
+    ArrayList<Bag> packingList = new ArrayList<>();
     DataReader dr = new DataReader();
 
-    int BagEndurance = 10;
 
 
     public void packProducts() {
@@ -19,6 +19,7 @@ public class ProductPacker {
         Collections.sort(listOfProducts);       //sort list by the weight
         Collections.reverse(listOfProducts);    //invert it so heaviest is first
 
+        Bag bag = new Bag(1);
 
         int bagCounter = 1;
         int bagWeight = 0;
@@ -33,23 +34,29 @@ public class ProductPacker {
         int prodCounter = 0;
 
         while (prodCounter != shoppingList.size()) {
-            if (bagWeight == BagEndurance){
+            if (bagWeight == bag.getWeightEndurance()){
                 bagWeight = 0;
                 bagCounter++;
+                packingList.add(bag);
+                bag = new Bag(bagCounter);
             }
             for (Product listProduct : listOfProducts) {
                 for (ShoppingList cartProduct : shoppingList) {
                     if (cartProduct.isPacked()) continue;
                     if (cartProduct.getID() == listProduct.getID()) {
-                        if (bagWeight + listProduct.getProduct_Weight() <= BagEndurance) {
+                        if (bagWeight + listProduct.getProduct_Weight() <= bag.getWeightEndurance()) {
                             bagWeight = bagWeight + listProduct.getProduct_Weight();
                             cartProduct.setPacked(true);
+                            bag.addProductToBag(listProduct);
                             prodCounter++;
                         } else if (cartProduct.isTriedToPack()){
                             bagWeight = 0;
                             bagCounter++;
                             bagWeight = bagWeight + listProduct.getProduct_Weight();
                             cartProduct.setPacked(true);
+                            bag.addProductToBag(listProduct);
+                            packingList.add(bag);
+                            bag = new Bag(bagCounter);
                             prodCounter++;
                         }
                         else cartProduct.setTriedToPack(true);
@@ -57,9 +64,17 @@ public class ProductPacker {
                 }
             }
         }
+        if (bagWeight != 0){
+            packingList.add(bag);
+        }
 
         System.out.println("Number of used bags is " + bagCounter);
         System.out.println("Total weight of products is " + totalWeight);
+        for (Bag sBag : packingList)
+        {
+            System.out.println("Products in bag nr " + sBag.getBagNumber() + ":");
+            sBag.listAllProductsInBag();
+        }
     }
 
 
